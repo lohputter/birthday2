ca1376ad5edd81dd89a5bb91b56226da4433c862
Lyndon Loh <129501406+lohputter@users.noreply.github.com>
2023-06-03 01:40:22 +0800

    Revert "yes"
    This reverts commit 6412bfba4dd0cf8f0ea49de5fbea724fcc4ccc97.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dist/",
+  base: "/birthday2/",
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
-  },
 })
