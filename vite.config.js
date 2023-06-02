cdd57c29a155493e9b14b1e8e40f952bc7dfd2b2
Lyndon Loh <129501406+lohputter@users.noreply.github.com>
2023-06-03 01:53:15 +0800

    23

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dist/",
+  base: "/birthday2/dist/",
   plugins: [react()],
 })
